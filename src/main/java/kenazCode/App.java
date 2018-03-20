c722dfc90daaa4d389e4a1491d4b32e4d6228f8a
zanek <cekarcekar@gmail.com>
2018-03-20 10:52:45 +0100

    main puzzle

---
+++ src/main/java/kenazCode/App.java
@@ -0,0 +1,13 @@
+package kenazCode;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
